a338d637d1b8e76851e1a1ba00209610409a5462
TheVTM <tallibba@gmail.com>
2015-10-19 19:50:49 -0200

    Git Submodules adicionados a pre instalacao

--- bin/preinstall.js
+++ bin/preinstall.js
@@ -3,6 +3,10 @@ var resolve = require('path').resolve;
 var join = require('path').join;
 var cp = require('child_process');
 
+// GIT Submodules
+cp.spawnSync('git', ['submodule', 'init'], { env: process.env, cwd: __dirname, stdio: 'inherit' });
+cp.spawnSync('git', ['submodule', 'update'], { env: process.env, cwd: __dirname, stdio: 'inherit' });
+
 // get library path
 var lib = resolve(__dirname, '../lib/');
 
@@ -16,4 +20,4 @@ fs.readdirSync(lib).forEach(function (mod) {
 
   // install folder
   cp.spawn('npm', ['i'], { env: process.env, cwd: modPath, stdio: 'inherit' });
-})
+});
